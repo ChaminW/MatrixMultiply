308311bdbff092b0f78c5e68a41c9ff6c81abbe3
chaminw <chamin.13@cse.mrt.ac.lk>
2017-06-19 13:05:14 +0530

    Add optimization 1

--- matMultiplyOpt1.cpp
+++ matMultiplyOpt1.cpp
@@ -58,10 +58,17 @@ void multiplyMatParallel(vector< vector<double> > &a,vector< vector<double> > &b
 		}
 	}
 	
-	
+void transpose(vector< vector<double> > &b, vector< vector<double> > &btrans, int n) {
+	//Finding transpose of matrix a[][] and storing it in array trans[][]
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < n; ++j)
+        {
+            btrans[j][i]=b[i][j];
+        }
+}
 int main()
 {
-	cout << "Sequential multiplication"<< endl;
+	/*cout << "Sequential multiplication"<< endl;
 	for (int n = 200; n <= 2000; n+=200) {
 		double total_time = 0;
 		for (int k = 0; k < 20; k++) {
@@ -94,6 +101,43 @@ int main()
 		}
 		cout << "Average time taken to execute in n-"<< n << " : "<< total_time/20 << endl;
 	}
+	*/
+	cout << "Sequential multiplication with transpose - optimization 1"<< endl;
+	for (int n = 200; n <= 2000; n+=200) {
+		double total_time = 0;
+		for (int k = 0; k < 20; k++) {
+			vector< vector<double> > a(n,vector<double>(n)),b(n,vector<double>(n)),btrans(n,vector<double>(n)),c(n,vector<double>(n));	//c = a * b, c is the result matrix
+			
+			initMat(a,b,n);
+			
+			dtime = omp_get_wtime();
+			transpose(b,btrans,n);
+			multiplyMatSeq(a,btrans,c,n);
+			dtime = omp_get_wtime() - dtime;
+			//cout << "Time taken to execute in n-"<< n << " : "<< dtime << endl;
+			total_time+= dtime;
+		}
+		cout << "Average time taken to execute in n-"<< n << " : "<< total_time/20 << endl;
+	}
+	
+	cout << "Parallel multiplication using openMP with transpose - optimization 1"<< endl;
+	for (int n = 200; n <= 2000; n+=200) {
+		double total_time = 0;
+		for (int k = 0; n < 20; n++) {
+			vector< vector<double> > a(n,vector<double>(n)),b(n,vector<double>(n)),btrans(n,vector<double>(n)),c(n,vector<double>(n));	//c = a * b, c is the result matrix
+			
+			initMat(a,b,n);
+			
+			dtime = omp_get_wtime();
+			transpose(b,btrans,n);
+			multiplyMatParallel(a,btrans,c,n);
+			dtime = omp_get_wtime() - dtime;
+			//cout << "Time taken to execute in n-"<< n << " : "<< dtime << endl;
+			total_time+= dtime;
+		}
+		cout << "Average time taken to execute in n-"<< n << " : "<< total_time/20 << endl;
+	}
+	
     return 0;
 }
 
